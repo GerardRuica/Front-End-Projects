ab221874b9fb7ae6d0f2afd3cedd171ebb41aa23
GerardRuica <gerard.ruica@gmail.com>
2024-09-22 15:05:24 +0200

    Bug navbarbar fixed
    Error that appears when clicking on an element of the navigation bar when you are in a section below and you try to go to the section above, the scroll bar does not disappear

--- 03-personal-portafolio/js/scroll.js
+++ 03-personal-portafolio/js/scroll.js
@@ -1,15 +1,29 @@
-let lastScrollTop = 0; // Guarda la posición del último scroll
+let lastScrollTop = 0;
+let disableScrollCheck = false;
+const navBar = document.querySelector(".nav-bar");
+const links = document.querySelectorAll(".nav-bar a");
 
 window.addEventListener("scroll", function () {
-  const navBar = document.querySelector(".nav-bar");
   const currentScroll =
     window.pageYOffset || document.documentElement.scrollTop;
 
+  if (disableScrollCheck) return;
   if (currentScroll > lastScrollTop) {
     navBar.classList.add("hide");
-  } else {
+  } else if (currentScroll < lastScrollTop && currentScroll > 0) {
     navBar.classList.remove("hide");
   }
 
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    navBar.classList.add("hide");
+    disableScrollCheck = true;
+
+    setTimeout(() => {
+      disableScrollCheck = false;
+    }, 1000);
+  });
 });
